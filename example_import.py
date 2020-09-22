55aefe94043c25ca943cfe22146d96981564c84d
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-09-22 10:17:27 -0400

    finished ch 8

---
+++ example_import.py
@@ -0,0 +1,11 @@
+import example_module
+
+print(example_module.example_function('The Long Run', "Eagles"))
+
+# alternative import styles 
+
+from example_module import example_function
+from example_module import example_function as ef 
+import example_module as em 
+from example_module import *
+
---
+++ example_module.py
@@ -0,0 +1,4 @@
+def example_function(album, artist):
+    return f"Hey have you heard {album}, by the {artist} yet?"
+
+
